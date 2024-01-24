5e68d72de841d1c89e8024b9abae4f118be58a53
ajarrra <ajarka1334@gmail.com>
2024-01-24 15:47:19 +0600

    finished my project

--- js/core.js
+++ js/core.js
@@ -11,4 +11,9 @@ console.log(fibonacci(1)); // 1
 console.log(fibonacci(2)); // 1
 console.log(fibonacci(3)); // 2
 console.log(fibonacci(4)); // 3
-console.log(fibonacci(5)); // 5
+console.log(fibonacci(5)); 
+console.log(fibonacci(6));
+console.log(fibonacci(7));
+console.log(fibonacci(8));
+console.log(fibonacci(9));
+console.log(fibonacci(10));
